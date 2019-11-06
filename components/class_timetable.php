d641917ddbc6e0a9cb634bf930d133f2c1bcb569
Léonard Noth <leonardnikita.noth@edu.hefr.ch>
2019-11-06 14:20:04 +0100

    merge timeslots with several teachers

--- components/class_timetable.php
+++ components/class_timetable.php
@@ -3,7 +3,7 @@
 if (!isset($_GET['classId'])) {
     die('Error : no classId specified');
 }
-if (intval($_GET['classId'] === 0)) {
+if (intval($_GET['classId']) === 0) {
     die('Error : wrong classId');
 }
 
@@ -23,6 +23,28 @@ if (count($concerned_weekdays) === 0) {
 usort($class_timetable, function($x, $y) {
     return $x['timeslot_id'] - $y['timeslot_id'];
 });
+
+$registrations_table = array();
+$timeslots_ids_keys = array();
+$timeslots_ids = array();
+
+foreach ($class_timetable as $ct) {
+    $regId = 'id_'.$ct['registration_id'];
+    $timeslotId = 'id_'.$ct['timeslot_id'];
+    if (!array_key_exists($regId, $registrations_table)) {
+        $registrations_table[$regId] = array();
+    }
+    if (!array_key_exists($timeslotId, $registrations_table[$regId])) {
+        $timeslots_ids_keys[$timeslotId] = true;
+        $registrations_table[$regId][$timeslotId] = array();
+    }
+    array_push($registrations_table[$regId][$timeslotId], $ct);
+}
+
+foreach($timeslots_ids_keys as $tkey=>$tval) {
+    array_push($timeslots_ids, $tkey);
+}
+sort($timeslots_ids);
 
 $activeWeekday = intval(date("N", time()));
 $activeWeekdayInList = false;
@@ -53,11 +75,11 @@ while ($activeWeekdayInList === false) {
                     </a>
                 </li>
             <?php } ?>
-            <li class="nav-item">
+            <!--<li class="nav-item">
                 <a class="nav-link" id="week-tab" data-toggle="tab" href="#week" role="tab" aria-controls="week" aria-selected="true">
                     Toute la semaine
                 </a>
-            </li>
+            </li>-->
         </ul>
     </div>
     <div class="tab-content">
@@ -74,21 +96,39 @@ while ($activeWeekdayInList === false) {
                     </tr>
                     </thead>
                     <tbody>
-                    <?php foreach($class_timetable as $class_timeslot) {
-                        if ($class_timeslot['weekday_name_en'] === $wname) { ?>
-                            <tr>
-                                <td><?= $class_timeslot['start_hour'] . ' - ' . $class_timeslot['end_hour'] ?></td>
-                                <td><?= $class_timeslot['course_name'] ?></td>
-                                <td><?= $class_timeslot['teacher_name'] ?></td>
-                                <td><?= $class_timeslot['room_name'] ?></td>
-                            </tr>
-                        <?php }
+                    <?php foreach($registrations_table as $registration) {
+                        foreach($timeslots_ids as $timeslot_id) {
+                            if (array_key_exists($timeslot_id, $registration)) {
+                                if ($registration[$timeslot_id][0]['weekday_name_en'] === $wname) {
+                                    $teachers = array();
+                                    foreach($registration[$timeslot_id] as $pr) {
+                                        array_push($teachers, $pr['teacher_name']);
+                                    }
+                                    $el = $registration[$timeslot_id][0]; ?>
+                                    <tr>
+                                        <td><?= $el['start_hour'] . ' - ' . $el['end_hour'] ?></td>
+                                        <td><?= $el['course_name'] ?></td>
+                                        <td><!--<?php for ($i = 0; $i < count($teachers); $i++) {
+                                            $teacher = $teachers[$i];
+                                                echo $teacher;
+                                                if ($i + 1 < count($teachers)) {
+                                                    echo ', ';
+                                                };
+                                            }
+                                        ?>-->
+                                        <?= count($teachers) > 1 ? 'plusieurs professeurs' : $teachers[0] ?>
+                                        </td>
+                                        <td><?= $el['room_name'] ?></td>
+                                    </tr>
+                                <?php }
+                            }
+                        }
                     } ?>
                     </tbody>
                 </table>
             </div>
         <?php } ?>
-        <div class="text-center p-3 tab-pane fade show" id="week" role="tabpanel" aria-labelledby="week-tab">
+        <!--<div class="text-center p-3 tab-pane fade show" id="week" role="tabpanel" aria-labelledby="week-tab">
             <table class="table table-striped table-bordered">
                 <thead>
                 <tr>
@@ -100,43 +140,9 @@ while ($activeWeekdayInList === false) {
                 </tr>
                 </thead>
                 <tbody>
-                <?php $weekdays_shown = array();
-                $isFirstWeekday = true;
-                foreach($concerned_weekdays as $wname => $concerned_weekday) {
-                    foreach($class_timetable as $class_timeslot) {
-                        if ($class_timeslot['weekday_name_en'] === $wname) {
-                            if (!array_key_exists($wname, $weekdays_shown)) {
-                                $weekdays_shown[$wname] = true;
-                                if (!$isFirstWeekday) { ?>
-                                    <tr>
-                                        <td></td><td></td><td></td><td></td><td></td>
-                                    </tr>
-                                <?php $isFirstWeekday = false;
-                                } ?>
-                                <tr>
-                                    <td><?= $concerned_weekday[1] ?></td>
-                                    <td><?= $class_timeslot['start_hour'] . ' - ' . $class_timeslot['end_hour'] ?></td>
-                                    <td><?= $class_timeslot['course_name'] ?></td>
-                                    <td><?= $class_timeslot['teacher_name'] ?></td>
-                                    <td><?= $class_timeslot['room_name'] ?></td>
-                                </tr>
-                            <?php } else { ?>
-                            <tr>
-                                <td><?php if (!array_key_exists($wname, $weekdays_shown)) {
-                                    $weekdays_shown[$wname] = true;
-                                    echo $concerned_weekday[1];
-                                } ?></td>
-                                <td><?= $class_timeslot['start_hour'] . ' - ' . $class_timeslot['end_hour'] ?></td>
-                                <td><?= $class_timeslot['course_name'] ?></td>
-                                <td><?= $class_timeslot['teacher_name'] ?></td>
-                                <td><?= $class_timeslot['room_name'] ?></td>
-                            </tr><?php
-                            }
-                        }
-                    }
-                } ?>
+                    <tr></tr>
                 </tbody>
             </table>
-        </div>
+        </div>-->
     </div>
 </div>
--- static/js/index.js
+++ static/js/index.js
@@ -4,12 +4,15 @@ $(function() {
            action: 'get_class_timetable',
            classId: $('#timetable_class_choice').val()
        })
-           .done(function(result) {
-               if (result.includes('Error : ') && !result.includes('<')) {
-                   $('#search_results').html('<div class="d-flex"><div class="ml-auto mr-auto alert alert-danger">' + result + '</div></div>');
-               } else {
-                   $('#search_results').html(result);
-               }
-           })
+       .done(function(result) {
+           if (result.includes('Error : ') && !result.includes('<')) {
+               $('#search_results').html('<div class="d-flex"><div class="ml-auto mr-auto alert alert-danger">' + result + '</div></div>');
+           } else {
+               $('#search_results').html(result);
+           }
+       })
+       .fail(function() {
+           $('#search_results').html('<div class="d-flex"><div class="ml-auto mr-auto alert alert-danger">Error : results unavailable</div></div>');
+       });
     });
 });
